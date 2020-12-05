e7f1df0742629c7c323df5e17a168a1a42a5bbce
Emmy Steven <74903385+emmysteven@users.noreply.github.com>
2020-12-05 23:55:38 +0100

    Setting up my first unit test

--- WebUI/ClientApp/src/app/app.js
+++ WebUI/ClientApp/src/app/app.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 // import './app.css';
-import { Header, Footer, Routes } from "../components";
-import { userService } from '../services'
-import { UserContext } from '../utils'
+import { Header, Footer, Routes } from "../components/index";
+import { userService } from '../services/index'
+import { UserContext } from '../utils/index'
 
 export function App() {
   const [userData, setUserData] = useState({
@@ -18,7 +18,7 @@ export function App() {
   return (
     <BrowserRouter>
       <UserContext.Provider value={{ userData, setUserData }}>
-        <div className="App">
+        <div className="App" data-testid="app">
           <Header/>
             { Routes }
           <Footer/>
--- WebUI/ClientApp/src/app/app.test.js
+++ WebUI/ClientApp/src/app/app.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
-import App from './app';
+import { App } from './app';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByTestId("app");
   expect(linkElement).toBeInTheDocument();
 });
