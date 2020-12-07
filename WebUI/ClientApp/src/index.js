8b5bb67f79c24fdb5b9f4489cd582f94456c6b3e
Emmy Steven <74903385+emmysteven@users.noreply.github.com>
2020-12-07 04:23:21 +0100

    Imported bootstrap.bundle.min

--- WebUI/ClientApp/src/index.js
+++ WebUI/ClientApp/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './index.css';
 import { App } from './app';
 
