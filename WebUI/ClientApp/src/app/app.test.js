1d97f699e459dd6c0172527641c3f9d4af3e3882
Emmy Steven <hello@emmysteven.com>
2020-12-24 04:07:25 +0100

    remove ClientApp from WebUI & make it a separate project

--- WebUI/ClientApp/src/app/app.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import { App } from './app';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByTestId("app");
-  expect(linkElement).toBeInTheDocument();
-});
