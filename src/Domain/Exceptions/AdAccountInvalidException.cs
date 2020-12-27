1455b7ecf94d52d70368305da0dc40a178009f0c
Emmy Steven <hello@emmysteven.com>
2020-12-28 00:32:17 +0100

    feat(adaccountinvalidexception): add exception for AdAccount
    fix #18

---
+++ src/Domain/Exceptions/AdAccountInvalidException.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Restaurant.Domain.Exceptions
+{
+    public class AdAccountInvalidException : Exception
+    {
+        public AdAccountInvalidException(string adAccount, Exception ex)
+            : base($"AD Account \"{adAccount}\" is invalid.", ex)
+        {
+        }
+    }
+}
