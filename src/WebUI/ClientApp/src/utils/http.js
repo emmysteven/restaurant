2124b30afc7da5dcfdb101a3f34872e628562576
Emmy Steven <hello@emmysteven.com>
2020-12-31 23:41:09 +0100

    fix(spa): change baseURL
    fix #29

--- src/WebUI/ClientApp/src/utils/http.js
+++ src/WebUI/ClientApp/src/utils/http.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const http = axios.create({
-  baseURL: 'https://127.0.0.1:3000/api',
+  baseURL: 'https://localhost:3000/api',
   crossdomain: true,
   headers: {
     'Content-Type': 'application/json; charset=utf-8',
