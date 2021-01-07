96c7f47446e21977c50d667a1a2c3ed4d9b13b7c
Emmy Steven <hello@emmysteven.com>
2021-01-07 13:04:35 +0100

    feat(domain): create value object for address
    fix #33

---
+++ src/Domain/ValueObjects/Address.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using Restaurant.Domain.Common;
+
+namespace Restaurant.Domain.ValueObjects
+{
+    public class Address : ValueObject
+    {
+        public String Street { get; private set; }
+        public String City { get; private set; }
+        public String State { get; private set; }
+
+        public Address() { }
+
+        public Address(string street, string city, string state)
+        {
+            Street = street;
+            City = city;
+            State = state;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            // Using a yield return statement to return each element one at a time
+            yield return Street;
+            yield return City;
+            yield return State;
+        }
+    }
+}
