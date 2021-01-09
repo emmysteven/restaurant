f07c62be32258731526b7365aac62aaa2fc9c54b
Emmy Steven <hello@emmysteven.com>
2021-01-10 00:13:30 +0100

    feat(domain): create a domain event
    fix #37

---
+++ src/Domain/Common/DomainEvent.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+namespace Restaurant.Domain.Common
+{
+    public interface IHasDomainEvent
+    {
+        public List<DomainEvent> DomainEvents { get; set; }
+    }
+    public abstract class DomainEvent
+    {
+        protected DomainEvent()
+        {
+            DateOccurred = DateTimeOffset.UtcNow;
+        }
+        public bool IsPublished { get; set; }
+        public DateTimeOffset DateOccurred { get; protected set; } = DateTime.UtcNow;
+    }
+}
