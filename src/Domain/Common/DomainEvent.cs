1e28ef7b9bf69889d29e20bb8bdf0e06a1f5d3fa
Emmy Steven <74903385+emmysteven@users.noreply.github.com>
2021-01-10 01:26:24 +0100

    feat(domain): create a domain event (#39)
    fix #37

---
+++ src/Domain/Common/DomainEvent.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+
+namespace Restaurant.Domain.Common
+{
+    public interface IHasDomainEvent
+    {
+        public List<DomainEvent> DomainEvents { get; set; }
+    }
+    public abstract class DomainEvent
+    {
+        protected DomainEvent()
+        {
+        }
+        public bool IsPublished { get; set; }
+    }
+}
