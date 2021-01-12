3c15f9d9736037c919fe5bc673b730bec9daf40d
Emmy Steven <hello@emmysteven.com>
2021-01-12 23:54:24 +0100

    feat(domain): create an event when a shop is created
    fix #40

---
+++ src/Domain/Events/ShopCreatedEvent.cs
@@ -0,0 +1,15 @@
+using Restaurant.Domain.Common;
+using Restaurant.Domain.Entities;
+
+namespace Restaurant.Domain.Events
+{
+    public class ShopCreatedEvent : DomainEvent
+    {
+        public ShopCreatedEvent(Shop item)
+        {
+            Item = item;
+        }
+        
+        public Shop Item { get; }
+    }
+}
