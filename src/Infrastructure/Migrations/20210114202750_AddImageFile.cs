5b68132eac830988a19cbd89c01fc1ee975c93e6
Emmy Steven <hello@emmysteven.com>
2021-01-17 20:46:59 +0100

    feat(migrations): create migration to reflect change in shop entity
    fix #48

---
+++ src/Infrastructure/Migrations/20210114202750_AddImageFile.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Restaurant.Infrastructure.Migrations
+{
+    public partial class AddImageFile : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageName",
+                table: "Shops",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageName",
+                table: "Shops");
+        }
+    }
+}
