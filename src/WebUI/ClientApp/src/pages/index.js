781a4151edc0e74494ef17f5eebcbcd8070c301a
Emmy Steven <hello@emmysteven.com>
2021-01-18 23:49:51 +0100

    feat(webui): remove react
    fix #52

--- src/WebUI/ClientApp/src/pages/index.js
+++
@@ -1,8 +0,0 @@
-export * from './register/register'
-export * from './contact/contact'
-export * from './about/about'
-export * from './login/login'
-export * from './error/error'
-export * from './home/home'
-export * from './start/start'
-
