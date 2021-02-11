1c9270599ccb7cc3e4dd71e453b15a3fe9de6b16
Emmy Steven <hello@emmysteven.com>
2021-02-11 16:28:18 +0100

    feat(ClientApp): display list of restaurants on homepage
    #fix 71

--- src/WebUI/ClientApp/src/pages/start/start.js
+++ src/WebUI/ClientApp/src/pages/start/start.js
@@ -1,16 +1,50 @@
-import { useHistory } from "react-router-dom";
+import { useState, useEffect } from 'react'
+import { ShopService } from '../../services/shop.service'
 
 export const Start = () => {
-  const history = useHistory();
+  const [shops, setShops] = useState([])
+
+  useEffect(() => {
+    ShopService.getAllShops().then(
+      (response) => {
+        setShops(response.data)
+      },
+      (error) => {
+        const _content =
+          (error.response && error.response.data) ||
+          error.message ||
+          error.toString()
+
+        setShops(_content)
+      }
+    )
+  }, [])
+
+  const list = shops.map(item => (
+      <div className='col-md-3 offset-md-0'  key={item.id}>
+        <div className='card'>
+          <img className='card-img-top' src={item.imagePath} alt='' />
+          <div className='card-body'>
+            <h5 className='card-title'>Name: {item.name}</h5>
+            <p className='card-text'>Website: {item.website}</p>
+            <p className='card-text'>Email: {item.email}</p>
+            <p className='card-text'>Phone: {item.phoneNumber}</p>
+            <p className='card-text'>State: {item.state}</p>
+            <p className='card-text'>LGA: {item.localGovernmentArea}</p>
+            <p className='card-text'>Address: {item.address}</p>
+          </div>
+        </div>
+      </div>
+    )
+  )
+
   return (
     <div>
-      <p className=''>
-        Want to start out with us? Well you&apos;re at the right place{' '}
-        <span role='img' aria-label='wizard'> 🧙‍♂️</span>
-      </p>
-      <button className='btn btn-dark mx-3 my-2' onClick={ () => {history.push('/login')} }>
-        Login
-      </button>
+        <div className=''>
+          <div className='card-deck'>
+            {list}
+          </div>
+        </div>
     </div>
   )
 }
