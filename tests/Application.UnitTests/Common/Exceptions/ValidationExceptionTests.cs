8c58fa4027a1fd8c0f04e0e7c647ecff3db732f7
Emmy <hello@emmysteven.com>
2022-01-11 14:35:17 +0100

    chore: use file scope namespace

--- tests/Application.UnitTests/Common/Exceptions/ValidationExceptionTests.cs
+++ tests/Application.UnitTests/Common/Exceptions/ValidationExceptionTests.cs
@@ -5,62 +5,61 @@ using FluentValidation.Results;
 using Restaurant.Application.Common.Exceptions;
 using Xunit;
 
-namespace Restaurant.Application.UnitTests.Common.Exceptions
+namespace Restaurant.Application.UnitTests.Common.Exceptions;
+
+public class ValidationExceptionTests
 {
-    public class ValidationExceptionTests
+    [Fact]
+    public void DefaultConstructorCreatesAnEmptyErrorDictionary()
     {
-        [Fact]
-        public void DefaultConstructorCreatesAnEmptyErrorDictionary()
+        var actual = new ValidationException().Errors;
+
+        actual.Keys.Should().BeEquivalentTo(Array.Empty<string>());
+    }
+
+    [Fact]
+    public void SingleValidationFailureCreatesASingleElementErrorDictionary()
+    {
+        var failures = new List<ValidationFailure>
         {
-            var actual = new ValidationException().Errors;
+            new ValidationFailure("Age", "must be over 18"),
+        };
 
-            actual.Keys.Should().BeEquivalentTo(Array.Empty<string>());
-        }
+        var actual = new ValidationException(failures).Errors;
 
-        [Fact]
-        public void SingleValidationFailureCreatesASingleElementErrorDictionary()
+        actual.Keys.Should().BeEquivalentTo(new string[] { "Age" });
+        actual["Age"].Should().BeEquivalentTo(new string[] { "must be over 18" });
+    }
+
+    [Fact]
+    public void MulitpleValidationFailureForMultiplePropertiesCreatesAMultipleElementErrorDictionaryEachWithMultipleValues()
+    {
+        var failures = new List<ValidationFailure>
         {
-            var failures = new List<ValidationFailure>
-            {
-                new ValidationFailure("Age", "must be over 18"),
-            };
+            new ValidationFailure("Age", "must be 18 or older"),
+            new ValidationFailure("Age", "must be 25 or younger"),
+            new ValidationFailure("Password", "must contain at least 8 characters"),
+            new ValidationFailure("Password", "must contain a digit"),
+            new ValidationFailure("Password", "must contain upper case letter"),
+            new ValidationFailure("Password", "must contain lower case letter"),
+        };
 
-            var actual = new ValidationException(failures).Errors;
+        var actual = new ValidationException(failures).Errors;
 
-            actual.Keys.Should().BeEquivalentTo(new string[] { "Age" });
-            actual["Age"].Should().BeEquivalentTo(new string[] { "must be over 18" });
-        }
+        actual.Keys.Should().BeEquivalentTo(new string[] { "Password", "Age" });
 
-        [Fact]
-        public void MulitpleValidationFailureForMultiplePropertiesCreatesAMultipleElementErrorDictionaryEachWithMultipleValues()
-        {
-            var failures = new List<ValidationFailure>
-            {
-                new ValidationFailure("Age", "must be 18 or older"),
-                new ValidationFailure("Age", "must be 25 or younger"),
-                new ValidationFailure("Password", "must contain at least 8 characters"),
-                new ValidationFailure("Password", "must contain a digit"),
-                new ValidationFailure("Password", "must contain upper case letter"),
-                new ValidationFailure("Password", "must contain lower case letter"),
-            };
+        actual["Age"].Should().BeEquivalentTo(new string[] 
+        { 
+            "must be 25 or younger", 
+            "must be 18 or older",
+        });
 
-            var actual = new ValidationException(failures).Errors;
-
-            actual.Keys.Should().BeEquivalentTo(new string[] { "Password", "Age" });
-
-            actual["Age"].Should().BeEquivalentTo(new string[] 
-            { 
-                "must be 25 or younger", 
-                "must be 18 or older",
-            });
-
-            actual["Password"].Should().BeEquivalentTo(new string[] 
-            { 
-                "must contain lower case letter",
-                "must contain upper case letter",
-                "must contain at least 8 characters",
-                "must contain a digit",
-            });
-        }
+        actual["Password"].Should().BeEquivalentTo(new string[] 
+        { 
+            "must contain lower case letter",
+            "must contain upper case letter",
+            "must contain at least 8 characters",
+            "must contain a digit",
+        });
     }
 }
