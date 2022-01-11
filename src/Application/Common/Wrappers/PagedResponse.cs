ee72a1d59423cc965b40d9186a4506adec7da86e
Emmy <hello@emmysteven.com>
2022-01-11 14:13:45 +0100

    chore: use file scope namespace

--- src/Application/Common/Wrappers/PagedResponse.cs
+++ src/Application/Common/Wrappers/PagedResponse.cs
@@ -1,18 +1,16 @@
-namespace Restaurant.Application.Common.Wrappers
+namespace Restaurant.Application.Common.Wrappers;
+public class PagedResponse<T> : Response<T>
 {
-    public class PagedResponse<T> : Response<T>
-    {
-        public int PageNumber { get; set; }
-        public int PageSize { get; set; }
+    public int PageNumber { get; set; }
+    public int PageSize { get; set; }
 
-        public PagedResponse(T data, int pageNumber, int pageSize)
-        {
-            this.PageNumber = pageNumber;
-            this.PageSize = pageSize;
-            this.Data = data;
-            this.Message = null;
-            this.Succeeded = true;
-            this.Errors = null;
-        }
+    public PagedResponse(T data, int pageNumber, int pageSize)
+    {
+        this.PageNumber = pageNumber;
+        this.PageSize = pageSize;
+        this.Data = data;
+        this.Message = null;
+        this.Succeeded = true;
+        this.Errors = null;
     }
 }
