be78080b24f9e7afe3133e026131ff2d78a1fe0e
Emmy <hello@emmysteven.com>
2022-01-11 14:09:50 +0100

    chore: use file scope namespace

--- src/Application/Common/Attributes/CacheAttribute.cs
+++ src/Application/Common/Attributes/CacheAttribute.cs
@@ -1,34 +1,33 @@
 using System;
 
-namespace Restaurant.Application.Common.Attributes
+namespace Restaurant.Application.Common.Attributes;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class CacheAttribute : Attribute
 {
-    [AttributeUsage(AttributeTargets.Class)]
-    public class CacheAttribute : Attribute
+    public string CacheKey;
+    public int Duration;
+    public TimeSpan TimeSpanForCacheInvalidation = TimeSpan.FromSeconds(60);
+
+    public CacheAttribute()
     {
-        public string CacheKey;
-        public int Duration;
-        public TimeSpan TimeSpanForCacheInvalidation = TimeSpan.FromSeconds(60);
+    }
 
-        public CacheAttribute()
-        {
-        }
+    public CacheAttribute(string cacheKey)
+    {
+        CacheKey = cacheKey;
+    }
 
-        public CacheAttribute(string cacheKey)
-        {
-            CacheKey = cacheKey;
-        }
+    public CacheAttribute(int duration)
+    {
+        Duration = duration;
+        TimeSpanForCacheInvalidation = TimeSpan.FromSeconds(duration);
+    }
 
-        public CacheAttribute(int duration)
-        {
-            Duration = duration;
-            TimeSpanForCacheInvalidation = TimeSpan.FromSeconds(duration);
-        }
-
-        public CacheAttribute(string cacheKey, int duration)
-        {
-            CacheKey = cacheKey;
-            Duration = duration;
-            TimeSpanForCacheInvalidation = TimeSpan.FromSeconds(duration);
-        }
+    public CacheAttribute(string cacheKey, int duration)
+    {
+        CacheKey = cacheKey;
+        Duration = duration;
+        TimeSpanForCacheInvalidation = TimeSpan.FromSeconds(duration);
     }
 }
